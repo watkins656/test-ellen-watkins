f77ee7bbc9a1208bdc5fcecc954f8a57fda33421
Dustin Watkins <watkins656@gmail.com>
2020-09-06 11:24:40 -0500

    loader

---
+++ client/public/assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    /*------------------------
+     loader
+     --------------------------*/
+     $('.loader-wrapper').fadeOut('slow', function() {
+            $(this).remove();
+        });
+});
--- client/src/Pages/bookAppointment.js
+++ client/src/Pages/bookAppointment.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
 import '../App.css';
 import logo from '../img/placeholder.PNG';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import EllenNavbar from '../Components/Navbar/Ellen-Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="lg" variant="light" style={{ backgroundColor: '#fcd3db'   }}>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" style={{justifyContent:'center'}}>
-          <Nav style={{flexGrow:1, flexBasis:0,justifyContent:'center'}}>
-            {/* <Nav.Link href="services">Skin Services and Treatments</Nav.Link> */}
-          </Nav>
-          <Nav style={{flexDirection: 'column',flexGrow:1, flexBasis:0,justifyContent:'center'}}>
-            <Navbar.Brand href="/"  style={{fontSize:'x-large'}}>Ellen Watkins Esthetics</Navbar.Brand>
-            <NavDropdown title="Luxury Skin Services and Treatments" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="services/#book">Book Appointment</NavDropdown.Item>
-              <NavDropdown.Item href="services/#treatments">Services + Treatments</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="services/#add-ons">About Ellen</NavDropdown.Item>
-              <NavDropdown.Item href="booking">Contact</NavDropdown.Item>
-              <NavDropdown.Item href="booking">FAQ</NavDropdown.Item>
-            </NavDropdown>
-
-          </Nav>
-          <Nav style={{flexGrow:1, flexBasis:0,justifyContent:'center'}}>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <EllenNavbar />
       <div className="App-header">
         <img src={logo} className="ellen-logo" alt="logo" />
         <a
@@ -34,11 +13,11 @@ function App() {
           href="https://square.site/book/TSYFEFHW3V7VR/laser-no-razor-lee-s-summit-mo"
           target="_blank"
           rel="noopener noreferrer"
-          >
+        >
           Book Appointment Here
         </a>
         <p className='Booking-text'>
-            I take appointments inside of Laser, No Razor, located in Lee's Summit.  When booking an appointment, you will be taken to the Laser, No Razor booking site.  For spa policies and procedures, please read below.   
+          I take appointments inside of Laser, No Razor, located in Lee's Summit.  When booking an appointment, you will be taken to the Laser, No Razor booking site.  For spa policies and procedures, please read below.
         </p>
 
       </div>
