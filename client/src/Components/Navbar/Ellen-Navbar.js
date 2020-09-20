66f34e120b9316155a99d645ad541364ef2bf44b
Dustin Watkins <watkins656@gmail.com>
2020-09-19 21:36:24 -0500

    book-appointment

--- client/src/Components/Navbar/Ellen-Navbar.js
+++ client/src/Components/Navbar/Ellen-Navbar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 class EllenNavbar extends React.Component {
   render() {
-    return (        <Navbar collapseOnSelect expand="lg" variant="light" style={{ backgroundColor: '#fcd3db'   }}>
+    return (        <Navbar collapseOnSelect expand="lg" variant="light" style={{ backgroundColor: '#fcd3db' }}>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav" style={{justifyContent:'center'}}>
       <Nav style={{flexGrow:1, flexBasis:0,justifyContent:'center'}}>
@@ -11,7 +11,7 @@ class EllenNavbar extends React.Component {
       </Nav>
       <Nav style={{flexDirection: 'column',flexGrow:1, flexBasis:0,justifyContent:'center'}}>
         <Navbar.Brand href="/"  style={{fontSize:'x-large'}}>Ellen Watkins Esthetics</Navbar.Brand>
-        <NavDropdown title="Luxury Skin Services and Treatments" id="collasible-nav-dropdown">
+        <NavDropdown title="Luxury Skin Services + Treatments" id="collasible-nav-dropdown">
           <NavDropdown.Item href="book-appointment">Book Appointment</NavDropdown.Item>
           <NavDropdown.Item href="services">Services + Treatments</NavDropdown.Item>
           <NavDropdown.Divider />
--- client/src/Pages/bookAppointment.js
+++ client/src/Pages/bookAppointment.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import '../App.css';
-import logo from '../img/placeholder.PNG';
+import logo from '../img/ellen-at-lazer.png';
 import EllenNavbar from '../Components/Navbar/Ellen-Navbar';
 function App() {
   return (
     <div className="App">
       <EllenNavbar />
-      <div className="App-header">
-        <img src={logo} className="ellen-logo" alt="logo" />
+      <div className="book-appointment-header">
+        <img src={logo} className="ellen-booking-logo" alt="logo" />
         <a
           className="App-link"
           href="https://square.site/book/TSYFEFHW3V7VR/laser-no-razor-lee-s-summit-mo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Book Appointment Here
+          Click here to book with me at Laser, No Razor!
         </a>
         <p className='Booking-text'>
-          I take appointments inside of Laser, No Razor, located in Lee's Summit.  When booking an appointment, you will be taken to the Laser, No Razor booking site.  For spa policies and procedures, please read below.
+          I take appointments inside of Laser, No Razor, located in Lee's Summit.  When booking an appointment, you will be taken to the Laser, No Razor booking site.  If you are not sure which facial to book, the Custom Facial is a great place to start.  If this is your first appointment with me, please arrive 10-15 minutes prior to your appointment.  I'll see you soon!
         </p>
 
       </div>
