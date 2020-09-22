30ecf514147d2474133826aa256a6a312c617447
Dustin Watkins <watkins656@gmail.com>
2020-09-21 23:12:46 -0500

    video responsive sizing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from './img/ellen-logo.PNG';
 import video from './video/Video.mov';
+import videoWide from './video/Video-wide.mov';
+import videoNarrow from './video/Video-narrow.mov';
 import EllenNavbar from './Components/Navbar/Ellen-Navbar';
 
 function App() {
@@ -9,11 +11,20 @@ function App() {
     <div className="App">
 <EllenNavbar />  
       <div className="App-header">
+        <div className="fall">
+Put your best face forward: healthy, glowing skin starts here!
+                  </div>
       <video className="ellen-video"  controls>
       <source src={video} type="video/mp4" />
      </video>
+      <video className="ellen-video-wide"  controls>
+      <source src={videoWide} type="video/mp4" />
+     </video>
+      <video className="ellen-video-narrow"  controls>
+      <source src={videoNarrow} type="video/mp4" />
+     </video>
         <div className="fall">
-          Hi I'm Ellen Watkins, a licensed esthetician in Lee's Summit MO.  I'm passionate about working with others to achieve their skincare goals through facials and advanced skin treatments.  To learn more about what I offer, please check out my service menu.  
+          Hi, I'm Ellen Watkins, a licensed esthetician in Lee's Summit, MO.  I'm passionate about working with others to achieve their skincare goals through facials and advanced skin treatments.  To learn more about what I offer, please check out my service menu.  
                   </div>
         <div className="luxury">
           luxury skin services + treatments
