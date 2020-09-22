2277fa99f5cb1ea56157d0ad6b383b523289b6d3
Dustin Watkins <watkins656@gmail.com>
2020-09-21 23:45:42 -0500

    about med

--- client/src/Pages/aboutEllen.js
+++ client/src/Pages/aboutEllen.js
@@ -14,9 +14,15 @@ function App() {
       <div id="menu-item-descriptions-wrapper">
       <img src={ellenFace} className="ellen-logo" alt="ellenFace" />
         <div id="menu-item-descriptions-title">
+        About Me
+          </div>
+          <p className='about-ellen-text'>
+Nice to meet you!  I am a KC native and have been passionate about skincare and personal health my entire life.  After a decade of frustration with my personal skin concerns (acne) I found esthetics.  I became fascinated with how the skin works and how to correct and treat skin concerns.  I know what it's like to be frustrated with your own skin and I help others feel confident with their own skin through results-oriented treatments.  My favorite things include wellness, cooking, and being active.  I live in Greenwood with my awesome husband and two little doves, Chocolate and Chip. 
+</p>
+        <div id="menu-item-descriptions-title">
           Mission Statement
           </div>
-          <p className='Booking-text'>
+          <p className='about-ellen-text'>
 
 Every face is different and skincare isn't one size fits all.  My philosophy is 'tailored skincare solutions.'  I help my clients set up homecare plans that work and provide luxury skin services and treatments to get everyone glowing like they've always wanted to.  I aim for meaningful relationships with my clients, and the goal for every experience is to make you feel pampered and cared for.
 </p>
