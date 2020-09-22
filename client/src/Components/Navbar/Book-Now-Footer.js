90bca935e9da5f0d7720ae7c44438a3aaa301d2e
Dustin Watkins <watkins656@gmail.com>
2020-09-22 01:27:04 -0500

    footer

--- client/src/Components/Navbar/Book-Now-Footer.js
+++ client/src/Components/Navbar/Book-Now-Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../../App.scss"
 import { Navbar, NavbarBrand, Container } from 'react-bootstrap';
 
 class EllenNavbar extends React.Component {
@@ -7,7 +8,7 @@ class EllenNavbar extends React.Component {
 <div className="footer-pink">
 <Navbar color="dark" dark>
   <Container>
-    <NavbarBrand style={{margin:"auto"}}><a href="https://square.site/book/TSYFEFHW3V7VR/laser-no-razor-lee-s-summit-mo">Book Now!</a></NavbarBrand>
+    <div className="book-now" style={{margin:"auto"}}><a href="https://square.site/book/TSYFEFHW3V7VR/laser-no-razor-lee-s-summit-mo">Book Now!</a></div>
   </Container>
 </Navbar>
 </div>
